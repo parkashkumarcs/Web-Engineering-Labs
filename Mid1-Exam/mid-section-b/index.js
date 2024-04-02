20aa0d04b652ceb121d6f58d9488785368c106eb
parkashkumarcs <parkashkumar.bscs.official@gmail.com>
2024-04-02 15:00:54 +0500

    done

---
+++ Mid1-Exam/mid-section-b/index.js
@@ -0,0 +1,6 @@
+const container=document.getElementById("myCont")
+
+container.onclick=function myImage(){
+    const myButton=document.getElementById("btn1")
+    myButton.onclick="<img src='two.svg'>"
+}
