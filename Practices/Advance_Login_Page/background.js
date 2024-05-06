d8c74c436dd9c94afce27d257236883a509a0c36
parkashkumarcs <parkashkumar.bscs.official@gmail.com>
2024-05-06 22:35:08 +0500

    done

---
+++ Practices/Advance_Login_Page/background.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // Set your custom background here
+    document.body.style.background = "url('bgIcon.png') no-repeat";
+    document.body.style.backgroundSize = "cover";
+    document.body.style.backgroundPosition = "center";
+  });
+  
